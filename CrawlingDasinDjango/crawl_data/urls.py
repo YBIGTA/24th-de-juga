21cbb9c1d600841aa46a4137f943f9e5be3459fd
hunny7434 <55790304+hunny7434@users.noreply.github.com>
2024-05-12 03:30:44 +0900

    feat:add crawling code

--- CrawlingDasinDjango/crawl_data/urls.py
+++ CrawlingDasinDjango/crawl_data/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import crawl_data, sample_response
+from .views import crawl_data
 
 urlpatterns = [
     path('crawl/', crawl_data, name='crawl-data'),
-    path('data/', sample_response, name='sample-response'),
 ]
--- CrawlingDasinDjango/crawl_data/views.py
+++ CrawlingDasinDjango/crawl_data/views.py
@@ -1,28 +1,41 @@
+from datetime import datetime, timedelta
 from django.shortcuts import render
-
-# Create your views here.
-
+import yfinance as yf
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 import json
+import numpy as np
+from datetime import datetime
 
+def add_business_days(start_date_str, add_days):
+    start_date = np.datetime64(start_date_str, 'D')
+    business_days_later = np.busday_offset(start_date, add_days, roll='forward')
+    return business_days_later.astype(datetime)
 
-def crawl_data(request):
-    # 여기에 크롤링 로직을 추가할 예정입니다.
-    data = {"message": "This is a placeholder response"}
-    return JsonResponse(data)
-
+def parse_date(date_str):
+    try:
+        return datetime.strptime(date_str, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Invalid date format. Please use YYYY-MM-DD.")
 @require_http_methods(["POST"])
-def sample_response(request):
+def crawl_data(request):
     try:
         data = json.loads(request.body)
-        code = data.get("code", "")
-        # 데이터 처리 로직
-        response_data = {
-            # "name": data.get("name", ""),
-            "2024-04-01 00:00": 4000,
-            "2024-04-01 00:05": 5000
-        }
-        return JsonResponse(response_data)
+        ticker = data.get("ticker", "")
+
+        start_date_str = data.get("start_date", "")
+        end_date = add_business_days(start_date_str, 5)
+
+        response_data = yf.download(ticker, start=start_date_str, end=end_date.strftime('%Y-%m-%d'))
+        if response_data.empty:
+            return JsonResponse({'error': 'No data available for the specified date range'}, status=404)
+
+        response_data.index = response_data.index.strftime('%Y-%m-%d')
+        return JsonResponse(response_data.to_dict())
+
     except json.JSONDecodeError:
         return JsonResponse({'error': 'Invalid JSON'}, status=400)
+    except ValueError as e:
+        return JsonResponse({'error': str(e)}, status=400)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
