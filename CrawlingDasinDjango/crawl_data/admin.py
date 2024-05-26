0fa0b45cf97735d501e83a6ca6eef750d8a546c8
hunny7434 <55790304+hunny7434@users.noreply.github.com>
2024-05-26 16:07:16 +0900

    fix: business day
    korea business day로 고침

--- CrawlingDasinDjango/crawl_data/admin.py
+++ CrawlingDasinDjango/crawl_data/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+# admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Holiday
+
+@admin.register(Holiday)
+class HolidayAdmin(admin.ModelAdmin):
+    list_display = ('date',)
+    search_fields = ('date',)
